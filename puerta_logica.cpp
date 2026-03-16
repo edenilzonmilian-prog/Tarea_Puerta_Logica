09e94d7ad1e5f95d8e22fab5e0716b423fb384e1
edenilzonmilian-prog <edenilzonmilian@gmail.com>
2026-03-15 23:25:31 -0600

    Add files via upload

---
+++ puerta_logica.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+
+    string nombre;
+    int edad;
+    double promedio;
+    int club;
+
+    cout << "Ingrese su nombre: ";
+    getline(cin, nombre);
+
+    if (nombre == "David Alvarez") {
+        cout << "Acceso especial concedido." << endl;
+    }
+    else {
+
+        cout << "Ingrese su edad: ";
+        cin >> edad;
+
+        cout << "Ingrese su promedio academico: ";
+        cin >> promedio;
+
+        cout << "Pertenece al club de programacion? (1=Si, 0=No): ";
+        cin >> club;
+
+        cout << fixed << setprecision(2);
+        cout << "Promedio ingresado: " << promedio << endl;
+
+        if ((edad >= 18 && promedio >= 85) || (club == 1 && promedio >= 75) || promedio >= 95) {
+            cout << "Bienvenido " << nombre << "." << endl;
+            cout << "Acceso autorizado." << endl;
+        }
+        else {
+            cout << "Acceso denegado." << endl;
+        }
+    }
+
+    return 0;
+}
